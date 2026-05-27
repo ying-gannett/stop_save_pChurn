6d37e13b9b250bb5d3341a42948346f82ef666c9
ying-gannett <ykang1@gannett.com>
2026-05-27 21:08:20 +0800

    Enhance monitor_performance.sql by adding payment tracking and refining subscription linking logic

--- src/sql/monitor_performance.sql
+++ src/sql/monitor_performance.sql
@@ -43,9 +43,10 @@ with raw as (  -- cleaned ss_test_applied
 lk as (
   SELECT distinct 
     lower(trim(l.billing_account)) as billing_account, 
-    l.circ_idsubscrip as id_subscrip
+    l.circ_idsubscrip as id_subscrip,
+    l.product_type
   from `gannett-enterprise-data.consumers_linkage_cz.subscription_link_latest` l 
-  where l.billing_system = 'ZUORA' and circ_site != 'PLAY' and product_type != 'E-Edition'
+  where l.billing_system = 'ZUORA' and circ_site != 'PLAY'
 ),
 ss_applied as (   -- link billing_account and id_subscrip
   select 
@@ -110,6 +111,41 @@ online as (   -- first confirmed online cancel after email date
     ss_applied.id_subscrip = cc.id_subscrip
     and cc.event_date >= ss_applied.email_date
 ),
+payment as (  
+  select *, coalesce(payment_amount, bill_amount) as amount
+  from (
+    SELECT 
+      lower(trim(p.account)) as billing_account,  p.id_subscrip,
+      invoice_number,
+      amount as bill_amount,
+      payment_amount,
+      id_payment_date,
+      case
+        WHEN id_payment_date is not null and id_decline_date is null then 'Paid' -- Normal payment
+        WHEN id_payment_date is not null and id_decline_date is not null and id_payment_date>=id_decline_date then 'Paid' -- Payment Date is after Decline
+        WHEN id_payment_date is null and id_decline_date is null and amount=0 and status='Posted' then 'Paid' -- First Invoice Free
+        WHEN id_payment_date is null and id_decline_date is not null then 'Not Paid' -- Normal decline
+        WHEN id_payment_date is not null and id_decline_date is not null and id_payment_date<id_decline_date then 'Not Paid' -- Payment reverse
+        WHEN id_payment_date is null and id_decline_date is null then 'Not Paid'
+        ELSE 'Other'
+      END AS payment_status,
+    FROM `gannett-enterprise-data.consumers_curated_zone_assets.subscriptions_invoice_payment` p
+    where id_payment_date >= '2026-04-03'
+  )
+  where payment_status = 'Paid' 
+),
+paid as (
+  select
+    ss_applied.billing_account, 
+    ss_applied.id_subscrip,
+    sum(p.amount) as tt_paid_amount
+  from ss_applied
+  join payment p on
+    ss_applied.billing_account = p.billing_account
+    and ss_applied.id_subscrip = p.id_subscrip
+    and p.id_payment_date >= ss_applied.email_date
+  group by 1, 2    
+),
 raw_combine as (
   select 
     ss_applied.*, 
@@ -121,6 +157,7 @@ raw_combine as (
     o.__ol_attempt_date,
     if(__ol_cancel_date is null, 0, 1) as online_canceled,
     o.__ol_cancel_date,
+    coalesce(p.tt_paid_amount, 0) as tt_paid_amount
   from ss_applied
   left join call_center c on
     ss_applied.billing_account = c.billing_account
@@ -128,27 +165,30 @@ raw_combine as (
   left join online o on
     ss_applied.billing_account = o.billing_account
     and ss_applied.id_subscrip = o.id_subscrip
+  left join paid p on
+    ss_applied.billing_account = p.billing_account
+    and ss_applied.id_subscrip = p.id_subscrip   
 )
-select distinct
-  b.*,
-  case 
-    when call_cancel_attempt+online_cancel_attempt=0 then 'No action yet'
-    when call_cancel_attempt+online_cancel_attempt=2 and call_cencelled+online_canceled=0 then 'Both attempt and Saved'
-      -- then if(__call_attempt_date>=__ol_attempt_date, 'Call Center Saved', 'Online Saved')
-    when call_cencelled+online_canceled=0 and call_cancel_attempt=1 then 'Call Center Saved'
-    when call_cencelled+online_canceled=0 and online_cancel_attempt=1 then 'Online Saved'
-    when call_cencelled+online_canceled=2 then 'Both Cancelled'
-      -- then if(__call_cancel_date<=__ol_cancel_date, 'Call Center Cancelled', 'Online Cancelled')
-    when call_cencelled=1 then 'Call Center Cancelled'
-    else 'Online Cancelled'
-  end as cancel_types,
-  if(call_cencelled+online_canceled=0, 0, 1) as churned,
-  y.risk_tier as src_risk_tier,
-  z.churn_truth
-from raw_combine b
-left join `gannett-datascience.test_activation_zone.stop_save_test_Bart` y on
-  lower(trim(y.billing_account)) = lower(trim(b.billing_account)) 
-  and y.inference_date = b.inference_date
-left join `gannett-enterprise-data.models_sz.source_pchurn_segments` z on
-  y.inference_date = z.inference_date
-  and y.id_subscrip = z.id_subscrip
+  select distinct
+    b.* except(id_subscrip),
+    case 
+      when call_cancel_attempt+online_cancel_attempt=0 then 'No action yet'
+      when call_cancel_attempt+online_cancel_attempt=2 and call_cencelled+online_canceled=0 then 'Both attempt and Saved'
+        -- then if(__call_attempt_date>=__ol_attempt_date, 'Call Center Saved', 'Online Saved')
+      when call_cencelled+online_canceled=0 and call_cancel_attempt=1 then 'Call Center Saved'
+      when call_cencelled+online_canceled=0 and online_cancel_attempt=1 then 'Online Saved'
+      when call_cencelled+online_canceled=2 then 'Both Cancelled'
+        -- then if(__call_cancel_date<=__ol_cancel_date, 'Call Center Cancelled', 'Online Cancelled')
+      when call_cencelled=1 then 'Call Center Cancelled'
+      else 'Online Cancelled'
+    end as cancel_types,
+    if(call_cencelled+online_canceled=0, 0, 1) as churned,
+    y.risk_tier as src_risk_tier,
+    z.churn_truth,
+  from raw_combine b
+  left join `gannett-datascience.test_activation_zone.stop_save_test_Bart` y on
+    lower(trim(y.billing_account)) = lower(trim(b.billing_account)) 
+    and y.inference_date = b.inference_date
+  left join `gannett-enterprise-data.models_sz.source_pchurn_segments` z on
+    y.inference_date = z.inference_date
+    and y.id_subscrip = z.id_subscrip
